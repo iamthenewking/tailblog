fc7f83d402a098d58e7904725d1e832762ccf2e1
iamthenewking <mrgabonijeff@gmail.com>
2021-06-22 19:20:32 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+import Link from './Link'
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="flex flex-col items-center mt-16">
+        <div className="flex mb-3 space-x-4">
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
+        </div>
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <Link href="/">{siteMetadata.title}</Link>
+        </div>
+         
+      </div>
+    </footer>
+  )
+}
